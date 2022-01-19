ce4413ca3a889e014b749593900f305e5ec9c69e
Benjie Friedman <benjiefriedman@gmail.com>
2022-01-19 15:36:00 -0800

    React app skeleton complete.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="information">
+        <label>First Name: </label>
+        <input type="text"></input>
+        <label>Last Name: </label>
+        <input type="text"></input>
+        <label>Email: </label>
+        <input type="email"></input>
+        <label>Phone: </label>
+        <input type="tel"></input>
+        <label>Company: </label>
+        <input type="text"></input>
+        <button>Add Personnel</button>
+      </div>
     </div>
   );
 }
