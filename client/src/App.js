31b0257faa46459b6e64227533ab2ecb75e2ac4f
Benjie Friedman <benjiefriedman@gmail.com>
2022-01-19 16:17:35 -0800

    Basic express connection set up.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,55 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [company, setCompany] = useState("");
+
   return (
     <div className="App">
       <div className="information">
         <label>First Name: </label>
-        <input type="text"></input>
+        <input
+          type="text"
+          onChange={(event) => {
+            setFirstName(event.target.value)
+          }}>
+        </input>
         <label>Last Name: </label>
-        <input type="text"></input>
+        <input
+          type="text"
+          onChange={(event) => {
+            setLastName(event.target.value)
+          }}>
+        </input>
         <label>Email: </label>
-        <input type="email"></input>
+        <input
+          type="email"
+          onChange={(event) => {
+            setEmail(event.target.value)
+          }}>
+        </input>
         <label>Phone: </label>
-        <input type="tel"></input>
+        <input
+          type="tel"
+          onChange={(event) => {
+            setPhone(event.target.value)
+          }}>
+        </input>
         <label>Company: </label>
-        <input type="text"></input>
-        <button>Add Personnel</button>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCompany(event.target.value)
+          }}>
+        </input>
+        <button>
+          Add Personnel
+        </button>
       </div>
     </div>
   );
